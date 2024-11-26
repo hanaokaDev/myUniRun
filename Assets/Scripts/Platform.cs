1c74ed2589c495810ad8de11cc1e50d6818e7b4e
User <user@DESKTOP-EDS44JG>
2024-11-27 00:45:56 +0900

    Added Feature: Score+=1

--- Assets/Scripts/Platform.cs
+++ Assets/Scripts/Platform.cs
@@ -8,9 +8,34 @@ public class Platform : MonoBehaviour {
     // 컴포넌트가 활성화될때 마다 매번 실행되는 메서드
     private void OnEnable() {
         // 발판을 리셋하는 처리
+        stepped = false;
+
+        for(int i = 0; i < obstacles.Length; i++)
+        {
+            // 1/3 확률로 장애물 활성화
+            if (Random.Range(0, 3) == 0) 
+            {
+                obstacles[i].SetActive(true);
+            }
+            else
+            {
+                obstacles[i].SetActive(false);
+            }
+        }
     }
 
     void OnCollisionEnter2D(Collision2D collision) {
         // 플레이어 캐릭터가 자신을 밟았을때 점수를 추가하는 처리
+        if(collision.collider.CompareTag("Player") && !stepped)
+        {
+            // 중복 점수 추가를 방지하기 위해 stepped 변수를 사용
+            Debug.Log("Score!");
+            stepped = true;
+            GameManager.instance.AddScore(1);
+        }
+        else
+        {
+            Debug.Log("No Score!");
+        }
     }
 }
